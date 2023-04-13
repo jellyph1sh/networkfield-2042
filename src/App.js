aaab81204d3a9bccfc11c718518a62480cd8e949
fevan <evan.ferron53@gmail.com>
2023-04-13 16:54:19 +0200

    :rocket: | create hacking player system

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 //import compenents
 import Mission from "./components/Mission";
 import WindowManager from "./components/WindowManager.js";
 import Shop from "./components/Shop.js";
 import TaskBarre from "./components/TaskBarre.js";
+import Window from "./components/Window.js";
 
 //import styles
 import "./styles/components/window.css";
@@ -17,13 +18,59 @@ import { playerData } from "./datas/playerObject.js";
 
 const playerTest = playerData;
 playerTest.name = "playerTest";
-playerTest.money = 9999;
+playerTest.money = 0.9;
 
 const App = () => {
+  let moneyStage = 1;
+  const [playerData, setPlayerData] = useState(playerTest);
+  const [showHackingWindow, setShowHackingWindow] = useState(false);
+  const styleWindowHack = useRef({
+    index: {
+      left: `${"400px"}`,
+      top: `${"200px"}`,
+      width: `${"500px"}`,
+      height: `${"600px"}`,
+    },
+  });
+
+  const hackPlayerMission = (
+    <Window
+      width={styleWindowHack.current.index.width}
+      height={styleWindowHack.current.index.height}
+      windowName={"hackPlayerWindow"}
+      setShowWindow={setShowHackingWindow}
+      styleWindow={styleWindowHack}
+      children={
+        <Mission
+          isTimer={true}
+          difficulty={playerData.currentLevel}
+          setShowWindow={setShowHackingWindow}
+        ></Mission>
+      }
+      canCloseWindow={false}
+    ></Window>
+  );
+
+  useEffect(() => {
+    if (playerData.money >= moneyStage) {
+      setShowHackingWindow(true);
+      moneyStage *= 10;
+    }
+  }, [playerData]);
+
   return (
     <div id="main-container">
-      {/* <Mission></Mission>
-      <h1>Hello Networkfield 2042!</h1> */}
+      {showHackingWindow ? hackPlayerMission : null}
+      <button
+        onClick={() => {
+          console.log(playerData.money);
+          setPlayerData((playerData) => ({
+            ...playerData,
+            ...{ money: playerData.money + 1 },
+          }));
+          console.log(playerData.money);
+        }}
+      ></button>
       <TaskBarre>
         <WindowManager
           width={`700px`}
@@ -32,7 +79,9 @@ const App = () => {
           top={"200px"}
           left={"400px"}
           urlIcon={"https://cdn-icons-png.flaticon.com/512/894/894117.png"}
-          children={<Shop playerData={playerTest}></Shop>}
+          children={
+            <Shop playerData={playerData} setPlayerData={setPlayerData}></Shop>
+          }
         ></WindowManager>
         <WindowManager
           width={`700px`}
--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,82 +1,121 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 import { getWords } from "../data/wordsMission.js";
 
-const Mission = ({difficulty=1, nbWords = 10, isTimer = false, timer=30, inputType="text"}) => {
-    const words = useRef(getWords(difficulty, nbWords));
-    const inputRef = useRef(null);
-    const [getCounter, setCounter] = React.useState(timer);
-    const index = useRef(0);
-    const [getWord, setWord] = useState(words.current[index.current]);
-    const [getErrorMessage, setErrorMessage] = useState("");
-    const [isFinish, setFinish] = useState(false);
+const Mission = ({
+  difficulty = 1,
+  nbWords = 10,
+  isTimer = false,
+  timer = 30,
+  inputType = "text",
+  setShowWindow,
+}) => {
+  const words = useRef(getWords(difficulty, nbWords));
+  const inputRef = useRef(null);
+  const [getCounter, setCounter] = React.useState(timer);
+  const index = useRef(0);
+  const [getWord, setWord] = useState(words.current[index.current]);
+  const [getErrorMessage, setErrorMessage] = useState("");
+  const [isFinish, setFinish] = useState(false);
 
-    const inputWord = <input ref={inputRef} type={inputType} name="enterWord" autoFocus autoComplete="off"/>
-    
-    React.useEffect(() => {
-        let timer;
-        if (getCounter > 0) {
-            timer = setTimeout(() => setCounter(c => c - 1), 1000);
-        }
-        return () => {
-            if (timer) {
-                clearTimeout(timer);
-            }
-        }
-    }, [getCounter]);
+  const inputWord = (
+    <input
+      ref={inputRef}
+      type={inputType}
+      name="enterWord"
+      autoFocus
+      autoComplete="off"
+    />
+  );
 
-    const checkIsWord = (userWord, correctWord) => {
-        if (userWord === correctWord) {
-            return true;
-        }
-        return false;
+  React.useEffect(() => {
+    let timer;
+    if (getCounter > 0) {
+      timer = setTimeout(() => setCounter((c) => c - 1), 1000);
     }
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, [getCounter]);
 
-    const checkWord = (e) => {
-        e.preventDefault();
+  const checkIsWord = (userWord, correctWord) => {
+    if (userWord === correctWord) {
+      return true;
+    }
+    return false;
+  };
 
-        console.log(checkIsWord(e.target.enterWord.value, getWord))
-        if (checkIsWord(e.target.enterWord.value, getWord)) {
-            if (index.current === words.current.length - 1) {
-                setFinish(true);
-            }
-            setErrorMessage("");
-            index.current = index.current + 1;
-            setWord(words.current[index.current]);
-        } else {
-            setErrorMessage("Wrong word!");
-        }
-        inputRef.current.value = "";
-    }
-    
-    if (isFinish) {
-        return (
-            <div>
-                <h1>DONE!</h1>
-            </div>
-        )
-    }
+  const checkWord = (e) => {
+    e.preventDefault();
 
-    if (isTimer === true && getCounter === 0 && !isFinish) {
-        return (
-            <div>
-                <h1>Time Over!</h1>
-                <h2>You lose!</h2>
-            </div>
-        )
+    console.log(checkIsWord(e.target.enterWord.value, getWord));
+    if (checkIsWord(e.target.enterWord.value, getWord)) {
+      if (index.current === words.current.length - 1) {
+        setFinish(true);
+      }
+      setErrorMessage("");
+      index.current = index.current + 1;
+      setWord(words.current[index.current]);
+    } else {
+      setErrorMessage("Wrong word!");
     }
+    inputRef.current.value = "";
+  };
+
+  if (isFinish) {
     return (
-        <div>
-            <form onSubmit={checkWord}>
-                {isTimer ? getCounter === 0 ? "" : <div>Countdown: {getCounter} seconds</div> : ""}
-                <label>
-                    {getWord}
-                    {inputWord}
-                    <input type="submit" />
-                </label>
-            </form>
-            <p>{getErrorMessage}</p>
-        </div>
+      <div>
+        <h1>DONE!</h1>
+        <button
+          onClick={() => {
+            setShowWindow(false);
+          }}
+          autoFocus
+        >
+          close
+        </button>
+      </div>
     );
+  }
+
+  if (isTimer === true && getCounter === 0 && !isFinish) {
+    return (
+      <div>
+        <h1>Time Over!</h1>
+        <h2>You lose!</h2>
+        <button
+          onClick={() => {
+            setShowWindow(false);
+          }}
+          autoFocus
+        >
+          close
+        </button>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <form onSubmit={checkWord}>
+        {isTimer ? (
+          getCounter === 0 ? (
+            ""
+          ) : (
+            <div>Countdown: {getCounter} seconds</div>
+          )
+        ) : (
+          ""
+        )}
+        <label>
+          {getWord}
+          {inputWord}
+          <input type="submit" />
+        </label>
+      </form>
+      <p>{getErrorMessage}</p>
+    </div>
+  );
 };
 
 export default Mission;
