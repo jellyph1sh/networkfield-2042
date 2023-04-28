08ef5d4cdcf46c1a8782f3d457e31a5d2d7e1d30
fevan <evan.ferron53@gmail.com>
2023-04-28 16:02:55 +0200

    complete hack player system and create generic stystem

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -12,6 +12,8 @@ const Mission = ({
   malus = false,
   setPlayerData,
   playerData,
+  winMessage,
+  looseMessage,
 }) => {
   const words = useRef(getWords(difficulty, nbWords));
   const inputRef = useRef(null);
@@ -21,7 +23,7 @@ const Mission = ({
   const [errorMessage, setErrorMessage] = useState(null);
   const [isFinish, setFinish] = useState(false);
   const allTry = useRef({ trials: [] });
-
+  // console.log("test reload hack window");
   const inputWord = (
     <input
       ref={inputRef}
@@ -36,6 +38,20 @@ const Mission = ({
     let timer;
     if (getCounter > 0) {
       timer = setTimeout(() => setCounter((c) => c - 1), 1000);
+    } else {
+      if (malus) {
+        console.log(
+          Math.round((playerData.money - playerData.money * 0.25) * 100) / 100
+        );
+        malus = false;
+        setPlayerData((playerData) => ({
+          ...playerData,
+          money:
+            Math.round((playerData.money - playerData.money * 0.25) * 100) /
+            100,
+        }));
+        console.log(playerData.money);
+      }
     }
     return () => {
       if (timer) {
@@ -44,8 +60,20 @@ const Mission = ({
     };
   }, [getCounter]);
 
+  const removeLastSpaces = (word) => {
+    let result = word;
+    while (result.length > 0) {
+      if (result[result.length - 1] == " ") {
+        result = result.slice(0, -1);
+      } else {
+        break;
+      }
+    }
+    return result;
+  };
+
   const checkIsWord = (userWord, correctWord) => {
-    if (userWord === correctWord) {
+    if (removeLastSpaces(userWord) === correctWord) {
       return true;
     }
     return false;
@@ -54,16 +82,23 @@ const Mission = ({
   const checkWord = (e) => {
     e.preventDefault();
     setErrorMessage(null);
-    allTry.current.trials.push("$ " + e.target.enterWord.value);
     console.log(checkIsWord(e.target.enterWord.value, getWord));
     if (checkIsWord(e.target.enterWord.value, getWord)) {
+      allTry.current.trials.push("$ " + e.target.enterWord.value);
       if (index.current === words.current.length - 1) {
         setFinish(true);
       }
       index.current = index.current + 1;
       setWord(words.current[index.current]);
     } else {
+      allTry.current.trials.push("$ Error : Invalid command");
       setErrorMessage(<p id="error-message">Wrong command!</p>);
+    }
+    if (allTry.current.trials.length > 19) {
+      allTry.current.trials = allTry.current.trials.splice(
+        1,
+        allTry.current.trials.length
+      );
     }
     inputRef.current.value = "";
   };
@@ -72,6 +107,7 @@ const Mission = ({
     return (
       <div id="mission-container-finished">
         <h1> DONE!</h1>
+        <h3 id="endMessage">{winMessage}</h3>
         <button
           onClick={() => {
             setShowWindow(false);
@@ -85,17 +121,11 @@ const Mission = ({
   }
 
   if (isTimer === true && getCounter === 0 && !isFinish) {
-    if (malus) {
-      setPlayerData((playerData) => ({
-        ...playerData,
-        money: playerData.money * 0.7,
-      }));
-    }
-
     return (
       <div id="mission-container-finished">
         <h1 className="loose-text">Time Over!</h1>
         <h2 className="loose-text">You lose!</h2>
+        <h3 id="endMessage">{looseMessage}</h3>
         <button
           onClick={() => {
             setShowWindow(false);
@@ -116,9 +146,18 @@ const Mission = ({
             <p>Countdown: {getCounter} seconds</p>
           </div>
         )
-      ) : null}
+      ) : (
+        <div id="countdown-container"></div>
+      )}
       <div id="trials-container">
         {Object.keys(allTry.current.trials).map((key, i) => {
+          if (allTry.current.trials[key] == "$ Error : Invalid command") {
+            return (
+              <li key={i} style={{ color: "red" }}>
+                {allTry.current.trials[key]}
+              </li>
+            );
+          }
           return <li key={i}>{allTry.current.trials[key]}</li>;
         })}
       </div>
