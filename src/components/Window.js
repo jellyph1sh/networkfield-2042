28edb2064e3cfb310eb874d20388359e22f295f8
fevan <evan.ferron53@gmail.com>
2023-05-07 11:44:21 +0200

    :zap: | rework window moving system

--- src/components/Window.js
+++ src/components/Window.js
@@ -1,47 +1,52 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import playSoundEffect from "../utils/playSoundEffect.js";
 import soundCloseWindow from "../sound/mouse-click2.mp3";
 
 const Window = ({
-  // width,
-  // height,
+  width,
+  height,
   windowName,
   setShowWindow,
   children,
   styleWindow,
   canCloseWindow = true,
 }) => {
-  // let moveInX, moveInY;
-  // const move = (event) => {
-  //   const element = document.getElementById(windowName);
-  //   element.style.left = `${event.pageX - moveInX}px`;
-  //   element.style.top = `${event.pageY - moveInY}px`;
-  //   styleWindow.current.index = {
-  //     left: element.style.left,
-  //     top: element.style.top,
-  //     width: width,
-  //     height: height,
-  //   };
-  // };
+  let moveInX, moveInY;
+  const move = (event) => {
+    const element = document.getElementById(windowName);
+    element.style.left = `${event.pageX - moveInX}px`;
+    element.style.top = `${event.pageY - moveInY}px`;
+    styleWindow.current.index = {
+      left: element.style.left,
+      top: element.style.top,
+      width: width,
+      height: height,
+    };
+  };
 
-  // const dragging = (event) => {
-  //   const element = event.target;
-  //   moveInX = event.clientX - element.getBoundingClientRect().left;
-  //   moveInY = event.clientY - element.getBoundingClientRect().top;
-  //   element.addEventListener("mousemove", move);
-  // };
+  const dragging = (event) => {
+    console.log("start moving");
+    const element = event.target;
+    moveInX = event.clientX - element.getBoundingClientRect().left;
+    moveInY = event.clientY - element.getBoundingClientRect().top;
+    document
+      .getElementById("main-container")
+      .addEventListener("mousemove", move);
+  };
 
-  // const freeze = (event) => {
-  //   const element = event.target;
-  //   element.removeEventListener("mousemove", move);
-  // };
+  const freeze = (event) => {
+    console.log("stop moving");
+    document
+      .getElementById("main-container")
+      .removeEventListener("mousemove", move);
+  };
 
   return (
     <div style={styleWindow.current.index} id={windowName} className="window">
       <div
         className="header-window"
-        // onPointerDown={dragging}
-        // onPointerUp={freeze}
+        onPointerDown={dragging}
+        onPointerUp={freeze}
       >
         <h3>{windowName}</h3>
         {canCloseWindow ? (
