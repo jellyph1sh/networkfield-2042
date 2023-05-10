9231510db223ff1f33ef7f973d5f4b4b1b8bbb37
Jellyph1sh <sacha.sorgiati@gmail.com>
2023-05-10 15:29:14 +0200

    :sparkles: | Add sector on map.

--- src/utils/createLocalStorage.js
+++ src/utils/createLocalStorage.js
@@ -1,7 +1,9 @@
 import data from "../data/missions.json";
+import countries from "../data/countries.json";
 
 const createLocalStorage = () => {
   localStorage.setItem("data", JSON.stringify(data));
+  localStorage.setItem("countries", JSON.stringify(countries));
   console.log(localStorage.getItem("data"));
 };
 
